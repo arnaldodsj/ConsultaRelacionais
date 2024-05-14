88be29a80708fdb9525657a7a2491a679b2b1f70
Arnaldo Dutra <117839610+arnaldodsj@users.noreply.github.com>
2024-05-14 10:57:05 -0300

    Add files via upload
    Desafio de Projeto de Estudos

---
+++ SQLQuery3.sql
@@ -0,0 +1,49 @@
+﻿---1 - Busca o nome e ano dos filmes--
+select nome, Ano from Filmes
+
+---2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano--
+select nome, Ano, Duracao from Filmes order by Ano asc
+
+---3--  
+select nome, Ano, Duracao from Filmes where nome = 'De Volta Para o Futuro' --poderia usar o like tbm
+
+---4--  
+select nome, Ano, Duracao from Filmes where Ano = '1997'
+
+---5--  
+select nome, Ano, Duracao from Filmes where Ano >= '2000'
+
+---6--  
+select nome, Ano, Duracao 
+from Filmes 
+where Duracao  > '100' and Duracao < '150' order by Duracao asc
+
+---7--  
+select ano,  count (*) as quantidade from Filmes group by Ano order by quantidade desc
+
+
+---8--  
+select PrimeiroNome, UltimoNome, Genero from Atores where Genero = 'm'
+
+
+---9--  
+select PrimeiroNome, UltimoNome, Genero from Atores where Genero = 'f' order by 1
+
+
+--10--
+SELECT Filmes.Nome, Generos.Genero FROM Filmes INNER JOIN
+FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme INNER JOIN
+Generos ON FilmesGenero.IdGenero = Generos.Id
+
+
+--11--
+SELECT Filmes.Nome, Generos.Genero FROM Filmes INNER JOIN
+FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme INNER JOIN
+Generos ON FilmesGenero.IdGenero = Generos.Id where Genero = 'Mist�rio'
+
+
+--12--
+SELECT        Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
+FROM            Atores INNER JOIN
+                         ElencoFilme ON Atores.Id = ElencoFilme.IdAtor INNER JOIN
+                         Filmes ON ElencoFilme.IdFilme = Filmes.Id
